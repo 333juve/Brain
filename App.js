0cc7e1f9181dad4ea81bb4b1ec141049d11d5e48
Alvaro Peña <alvpe26@gmail.com>
2022-04-23 16:04:30 -0500

    React Navigation Initial SetUp

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import SplashScreen from './src/screens/SplashScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import HomeScreen from './src/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator initialRouteName="Splash">
+        <StackScreen name="Splash" component={SplashScreen} />
+        <StackScreen name="LogIn" component={LogInScreen} />
+        <StackScreen name="SignUp" component={SignUpScreen} />
+        <StackScreen name="Home" component={HomeScreen} />
+      </StackNavigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
